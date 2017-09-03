35979d15b6b9cbb14ad0c848a97384f40db55811
Natasha Petrus <hello@natashapetrus.com>
2017-09-03 07:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getFirstSelector(selector){
   let match = document.querySelector(selector);
   return match;
 }
+
+function nestedTarget(){
+  let match = document.getElementById('nested').querySelector('.target');
+  return match;
+}
